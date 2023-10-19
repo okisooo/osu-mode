7dc29ecb41214c76e982de78d9803a541c233463
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-10-19 22:49:31 +0200

    Update

--- toggler.py
+++ toggler.py
@@ -5,6 +5,7 @@ import os
 import ctypes
 import sys
 import json
+from PIL import Image, ImageTk
 
 # Function to load the configuration from a JSON file
 def load_config():
@@ -20,15 +21,51 @@ def save_config(config):
     with open("config.json", "w") as config_file:
         json.dump(config, config_file)
 
+# Function to start moving the window
+def start_move(event):
+    global start_x, start_y
+    start_x = event.x
+    start_y = event.y
+
+# Function to stop moving the window
+def stop_move(event):
+    x = root.winfo_pointerx() - start_x
+    y = root.winfo_pointery() - start_y
+    root.geometry(f"+{x}+{y}")
+
+bg_image = Image.open("background.png")
+
+def resize_image(event):
+    new_width = event.width
+    new_height = event.height
+    image = bg_image.resize((new_width, new_height))
+    photo = ImageTk.PhotoImage(image)
+    bg_label.config(image=photo)
+    bg_label.image = photo
+
 # Initialize the Tkinter window
 root = tk.Tk()
 root.title("osu! Mode Switcher")
 window_width = 600
-window_height = 600
+window_height = 300
 root.geometry(f"{window_width}x{window_height}")
 root.resizable(False, False)  # The window won't be resizable
+root.bind('<Configure>', resize_image)
+root.configure(bg='#FFD1DC')  # Light pink
+root.overrideredirect(True)  # Remove the default title bar
+photo = ImageTk.PhotoImage(bg_image)
+bg_label = tk.Label(root, image=photo)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+bg_label.image = photo
 
-# Set the icon for the titlebar
+title_bar = tk.Frame(root, bg='pink', relief='raised', bd=2)  # Create a frame for the title bar
+title_bar.pack(fill=tk.X)  # Place the title bar at the top of the window
+title_label = tk.Label(title_bar, text="osu! Mode Switcher", bg='pink')  # Create a label for the title
+title_label.pack(side=tk.LEFT)  # Place the title on the left side of the title bar
+close_button = tk.Button(title_bar, text="X", bg='pink', command=root.destroy)  # Create a close button
+close_button.pack(side=tk.RIGHT)  # Place the close button on the right side of the title bar
+title_bar.bind("<Button-1>", start_move)
+title_bar.bind("<B1-Motion>", stop_move)
 icon_path = "C:\\Users\\samue\\Documents\\GitHub\\osudrawingtoggler\\icon.ico"  # Specify the path to your icon file
 root.iconbitmap(default=icon_path)
 
